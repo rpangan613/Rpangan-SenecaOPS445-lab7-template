a6fb6da94bf83916c14b76b1ed30544e79d07555
Roniel Pangan <roniel.pangan06@gmail.com>
2024-11-03 16:30:40 -0500

    Completed

--- lab7b.py
+++ lab7b.py
@@ -22,6 +22,34 @@ def sum_times(t1, t2):
     sum_time.minute = t1.minute + t2.minute
     sum_time.second = t1.second + t2.second
 
+def change_time(time, seconds):
+    time.second += seconds
+    # Handle overflow for seconds
+    while time.second < 0 or time.second >= 60:
+        if time.second < 0:
+            time.second += 60
+            time.minute -= 1
+        elif time.second >= 60:
+            time.second -= 60
+            time.minute += 1
+            
+    # Handle overflow for minutes
+    while time.minute < 0 or time.minute >= 60:
+        if time.minute < 0:
+            time.minute += 60
+            time.hour -= 1
+        elif time.minute >= 60:
+            time.minute -= 60
+            time.hour += 1
+            
+    # Optional: Handle hour overflow if you want
+    if time.hour < 0:
+        time.hour += 24
+    elif time.hour >= 24:
+        time.hour %= 24
+
+    return None
+
     # Carry over seconds to minutes
     if sum_time.second >= 60:
         sum_time.minute += sum_time.second // 60
